65df72e65d341a029aa39a8b21e1c116fd47f1ab
Dunstankiiru <alexkiiru7@gmail.com>
2026-02-14 21:45:14 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::post('/chirps', [ChirpController::class, 'store']);
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+
+//simpler way
+
+//Route::resource('/chirps', ChirpController::class)->only(['store', 'edit','update','destroy']);
