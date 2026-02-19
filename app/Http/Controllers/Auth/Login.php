5dbecaf3cc7b768d66fb56280527d4e5efb7d989
Dunstankiiru <alexkiiru7@gmail.com>
2026-02-19 10:44:38 +0300

    validates the input of login form

--- app/Http/Controllers/Auth/Login.php
+++ app/Http/Controllers/Auth/Login.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Controller
 {
@@ -12,6 +13,10 @@ class Login extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //Validates input
+        $credentials = $request->validate([
+            'email' => ['required|email'],
+            'password' => ['required'],
+        ]);
     }
 }
